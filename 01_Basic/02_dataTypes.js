69b06b3aa423f88d3ec26a1bfaa32b5dd86f2fb0
Vikas Singh <104720691+VikasSinghGitRepo@users.noreply.github.com>
2024-04-28 15:14:08 +0000

    Datatypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,34 @@
+"Use Strict"//treat all js code as a newer version
+
+//alert("vikas")  we are using nodejs, not browser
+
+
+//always maintain code readbility, it should be high always
+console.log("vikas  singh")
+console.log("vivek  singh")
+
+
+//these are seven data types
+let nameNull=null;
+let age=123
+let bigIntNo=123445555;
+let isChecked=false;
+let strname="vikas"
+//symbol
+let vikas=undefined
+
+//null == > it is a standalone value ( it means the value is empty)
+//undefined ==> it means that values is not assigned still
+
+//object is also a datatype
+
+typeof(vikas)
+
+//remember to learn more about it
+typeof(null) //object  
+typeof(undefined) //undefined
+
+
+
+
+
