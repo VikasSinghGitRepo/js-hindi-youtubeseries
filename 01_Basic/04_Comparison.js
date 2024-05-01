66d3b63e3fd347dd7862b4ad4417bdaa61f3dd00
Vikas Singh <104720691+VikasSinghGitRepo@users.noreply.github.com>
2024-05-01 04:18:08 +0000

    data type summary

---
+++ 01_Basic/04_Comparison.js
@@ -0,0 +1,20 @@
+//these are simple comarison operations
+console.log(2>1)
+console.log(2>=2)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+//difficult comparison is those where you compare between two different data types
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) // it behaves differently because equal operator converts null value to 0
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+//=== strict check
---
+++ 01_Basic/05_dataTypeSummary.js
@@ -0,0 +1,17 @@
+//data type is divided into only two types based on primitive and non primitive
+//Call by value and call by reference 
+// their difference is solely based on how you store that data in memoey and how they are accessed from the memory 
+
+//Primitive data types ( they are always call by value)
+//1.String
+//2.Number
+//3.Boolean
+//4.null
+//5.undefined
+//6.symbol
+//7.bigint
+
+//Reference type ( they are allocated reference not )
+//Array Objects Functions
+
+//Javascript is dynamically types language
